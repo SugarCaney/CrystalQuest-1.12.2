867a77c47857e34505acc404f58b51a0449fd247
SugarCaney <mrsugarcaney@gmail.com>
2019-03-31 12:52:58 +0200

    Fixed 1.12.2 compatibility issues.

--- src/nl/sugcube/crystalquest/CrystalQuest.java
+++ src/nl/sugcube/crystalquest/CrystalQuest.java
@@ -25,6 +25,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.logging.Level;
 
 /**
@@ -279,8 +280,9 @@ public class CrystalQuest extends JavaPlugin {
         data = YamlConfiguration.loadConfiguration(dataFile);
 
         InputStream defStream = this.getResource("data.yml");
+        InputStreamReader defStreamReader = new InputStreamReader(defStream);
         if (defStream != null) {
-            YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defStream);
+            YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defStreamReader);
             data.setDefaults(defConfig);
         }
     }
@@ -328,8 +330,9 @@ public class CrystalQuest extends JavaPlugin {
         lang = YamlConfiguration.loadConfiguration(langFile);
 
         InputStream defStream = this.getResource("lang.yml");
+        InputStreamReader defStreamReader = new InputStreamReader(defStream);
         if (defStream != null) {
-            YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defStream);
+            YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defStreamReader);
             lang.setDefaults(defConfig);
         }
     }
